dfb30a1eb400f32474d2384a4f09667530d7c613
Mikholae Baniaga <mikholae.baniaga@gmail.com>
2022-05-22 22:34:47 +0800

    api v1

--- mres-back-end/config/database.config.js
+++ mres-back-end/config/database.config.js
@@ -6,14 +6,9 @@ require('dotenv').config()
 const dbconnect = mysql2.createConnection({
     host: process.env.HOST,
     user: process.env.USER,
-<<<<<<< HEAD
     password: process.env.PASS,
     database: process.env.DATABASE,
     port: process.env.PORT,
-=======
-    database: process.env.DATABASE,
-    port: 3307
->>>>>>> 97b91aaa3a099de46160ddf873c10851b405b30c
 })
 
 dbconnect.connect(function(err){
